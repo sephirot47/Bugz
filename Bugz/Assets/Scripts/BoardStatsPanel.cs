eb15bf64d12b5492de0ca7a1a8d1bd16bcf36e02
sephirot47 <victorantondominguez@gmail.com>
2015-07-21 15:53:02 +0200

    Some canvases added. BoardGameController created. Tile Selection implemented. BoardStatsPanel created.

---
+++ Bugz/Assets/Scripts/BoardStatsPanel.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BoardStatsPanel : MonoBehaviour
+{
+    private static readonly string None = "-";
+
+    [SerializeField]
+    private Text lifeText, attackText, defenseText;
+
+	void Start () 
+    {
+        PopulateTexts(null);
+	}
+	
+	void Update () {
+	
+	}
+
+    public void PopulateTexts(Bug bug)
+    {
+        if(bug)
+        {
+            lifeText.text = bug.GetLife() + " / " + bug.GetMaxLife();
+            attackText.text = bug.GetAttack().ToString();
+            defenseText.text = bug.GetDefense().ToString();
+        }
+        else
+        {
+            lifeText.text = None;
+            attackText.text = None;
+            defenseText.text = None;
+        }
+    }
+}
